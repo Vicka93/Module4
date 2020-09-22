47178056d94be94b8918b19b3492d77999aee675
Vicka93 <57420845+Vicka93@users.noreply.github.com>
2020-09-22 17:40:01 -0500

    added input validation using try/except

--- input_validation/validation_with_try.py
+++ input_validation/validation_with_try.py
@@ -2,7 +2,7 @@
 Author:Viktoriia Denys
 Program:validation_with_try.py
 program validation_with_try.py reads in one person's names, first and last, their age and three scores out of 100.
-Taking the three scores and finding the average, storing into a variable.
+Taking the three scores and finding the average, storing into a variable.with added input validation using try/except
 """
 
 AMT_OF_SCORES = 3
@@ -20,8 +20,16 @@ if __name__ == '__main__':
     last_name = input("Enter your last name:")
     first_name = input("Enter your first name:")
     age = input("Enter your age:")
-    # Declaring a variable to store the answer to the function call
-    average_scores = average(10,90,100)
+    # Declaring a variable to store the answer to the function
+    first_score = input("Enter your your first score:")
+    second_score = input("Enter your your second score:")
+    third_score = input("Enter your your third score:")
+    try:
+        average_scores = average (int(first_score),int(second_score),int(third_score))
 
-    print("{}, {} age: {}  average grade: {}".format(last_name.capitalize(),
-          first_name.capitalize(), age, average_scores))
+    except ValueError:
+        print("\nPlease enter only positive values for scores")
+
+    else:
+        print("{}, {} age: {}  average grade: {:0.2f}".format(last_name.capitalize(), first_name.capitalize(),
+                                                         age, average_scores))
--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -2,7 +2,7 @@
 Author:Viktoriia Denys
 Program:validation_with_try.py
 program validation_with_try.py reads in one person's names, first and last, their age and three scores out of 100.
-Taking the three scores and finding the average, storing into a variable.
+Taking the three scores and finding the average, storing into a variable.with added input validation using try/except
 """
 
 import unittest
@@ -10,9 +10,17 @@ import unittest
 from input_validation import validation_with_try
 
 class MyTestCase(unittest.TestCase):
-    def test_average_exception(self):
+    def test_average_exception1(self):
         with self.assertRaises(ValueError):
             validation_with_try.average(-90, 89, 78)
+
+    def test_average_exception2(self):
+        with self.assertRaises(ValueError):
+            validation_with_try.average(90, - 89, 78)
+
+    def test_average_exception3(self):
+        with self.assertRaises(ValueError):
+            validation_with_try.average(90, 89, -78)
 
 
 if __name__ == '__main__':
